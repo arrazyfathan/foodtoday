9bebb11f068c8e02935ad83a17293c49fb57d050
Ar Razy Fathan Rabbani <arrazy.rabbani266@gmail.com>
2021-07-07 20:56:41 +0700

    add dark mode for home fragment

--- app/src/main/java/com/example/foody/ui/fragments/today/TodayFragment.kt
+++ app/src/main/java/com/example/foody/ui/fragments/today/TodayFragment.kt
@@ -1,11 +1,10 @@
 package com.example.foody.ui.fragments.today
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
@@ -66,11 +65,29 @@ class TodayFragment : Fragment() {
         return binding.root
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.food_joke_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.share_food_joke_menu) {
+            val shareIntent = Intent().apply {
+                this.action = Intent.ACTION_SEND
+                this.putExtra(Intent.EXTRA_TEXT, foodJoke)
+                this.type = "text/plain"
+            }
+
+            startActivity(shareIntent)
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
     private fun loadDataFromCache() {
         lifecycleScope.launch {
             mainViewModel.readFoodJoke.observe(viewLifecycleOwner, { database ->
                 if (database.isNotEmpty() && database != null) {
                     binding.foodJokeTextView.text = database[0].foodJoke.text
+                    foodJoke = database[0].foodJoke.text
                 }
             })
         }
